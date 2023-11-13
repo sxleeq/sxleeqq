21d26daa2691d2c247cdfab90857eace754d6c28
sxleeq <146856012+sxleeq@users.noreply.github.com>
2023-11-14 00:15:41 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+while a != 0 and b != 0:
+    if a > b:
+        a=a%b
+    else:
+        b = b % a
+while a != 0 and b != 0:
+    if a > b:
+    else:
+        b = b % a
+gcd = a + b
+print(gcd)
