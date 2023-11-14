82598810a5046f8a307588a6093d086f06d35bb9
sxleeq <146856012+sxleeq@users.noreply.github.com>
2023-11-14 17:19:24 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,18 +1,1 @@
-K = input()
-N = int(input()) - 1
 
-while N:
-    num1 = [K[0]]
-    k = 0
-    for i in range(1, len(K)):
-        if K[i] != K[i - 1]:
-            num1.append(K[i])
-            k += 1
-        else:
-            num1[k] += K[i]
-    K = ''
-    for z in num1:
-        K += str(len(z)) + z[0]
-    N -= 1
-
-print(' '.join(K))
